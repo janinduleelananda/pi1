868076f9d2615e74dd4ffbd8a3334cdcae73153c
janinduleelananda <janindur@gmail.com>
2020-04-02 17:39:51 +0530

    new

--- jan.py
+++ jan.py
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
+
+print("1")
 print("when the days are cold and the")
 #baby a u down down down down
 def print1(m):
     print(m)
+print("2")
 
-print1("jani")#nothing
-
->>>>>>> refs/remotes/origin/master
+print1("jani")#nothing can i type newsavcghsavghsvhgavshgvashgvhgasasshsjsm
