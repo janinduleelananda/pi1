424499676614438ab06f75741c0282ad83732f99
janinduleelananda <janindur@gmail.com>
2020-04-02 18:01:40 +0530

    new branch

--- jan.py
+++ jan.py
@@ -1,9 +1,7 @@
 
-print("1")
 print("when the days are cold and the")
 #baby a u down down down down
 def print1(m):
     print(m)
-print("2")
+print("ok")
 
-print1("jani")#nothing can i type newsavcghsavghsvhgavshgvashgvhgasasshsjsm
